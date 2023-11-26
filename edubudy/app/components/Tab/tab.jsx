c6f46e7dcfb04f24686bd93aeb6d20704cbc2236
Onur-Yılmaz <onuryilmaz@trair.com.tr>
2023-11-27 02:53:46 +0300

    added

--- edubudy/app/components/Tab/tab.jsx
+++ edubudy/app/components/Tab/tab.jsx
@@ -5,33 +5,29 @@ import { RiHeadphoneLine, RiUserVoiceLine, RiArticleLine, RiTeamLine } from 'rea
 
 const links = [
   { name: 'Dinle/Anlat', href: '/Features/eduCard', icon: <RiHeadphoneLine />, color: 'blue' },
-  { name: 'Kafam Karışık', href: '/Features/eduBlog', icon: <RiUserVoiceLine />, color: 'green' },
+  { name: 'Kafam/Karışık', href: '/Features/eduBlog', icon: <RiUserVoiceLine />, color: 'green' },
   { name: 'BLOG', href: '/Features/eduMentor', icon: <RiArticleLine />, color: 'yellow' },
-  { name: 'Meslek Erbabı', href: '/Features/eduSocial', icon: <RiTeamLine />, color: 'red' },
+  { name: 'Meslek/Erbabı', href: '/Features/eduSocial', icon: <RiTeamLine />, color: 'red' },
 ];
-
 function Navbar() {
-  const [activeLink, setActiveLink] = useState(null);
 
-  return (
-    <div class="bg-slate-400 py-2 container mx-auto mt-4 rounded-lg">
-        <ul className="flex justify-center space-x-6">
+    return (
+      <div className="bg-slate-400 py-5 px-20 w-min mx-auto mt-8 rounded-lg">
+        <ul className="flex justify-center items-center space-x-20 text-lg">
           {links.map((link, index) => (
             <li key={index}>
-              <a
-                className={`flex flex-col items-center text-white hover:text-gray-300 rounded-md p-2 ${activeLink === index ? `bg-${link.color}-700` : ''}`}
-                onClick={() => setActiveLink(index)}
-              >
-                <Link href={link.href}>
+
+                <Link className='border flex flex-col items-center ' href={link.href}>
                   {React.cloneElement(link.icon, { className: `text-${link.color}-500 text-2xl` })}
-                  <span className={`mt-2 ${activeLink === index ? `text-${link.color}-500` : ''}`}>{link.name}</span>
+                  <span className={`mt-auto`}>{link.name}</span>
                 </Link>
-              </a>
+ 
             </li>
           ))}
         </ul>
-    </div>
-  );
-}
-
-export default Navbar;
+      </div>
+    );
+  }
+  
+  export default Navbar;
+  
