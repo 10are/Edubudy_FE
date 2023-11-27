367e9b8bc4beb64193e87838ed5b47d7b705b8cf
Onur-Yılmaz <onuryilmaz@trair.com.tr>
2023-11-27 08:17:11 +0300

    added

--- edubudy/app/components/Tab/tab.jsx
+++ edubudy/app/components/Tab/tab.jsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 import { RiHeadphoneLine, RiUserVoiceLine, RiArticleLine, RiTeamLine } from 'react-icons/ri';
 
 const links = [
-  { name: 'Dinle/Anlat', href: '/Features/eduCard', icon: <RiHeadphoneLine />, color: 'blue' },
-  { name: 'Kafam/Karışık', href: '/Features/eduBlog', icon: <RiUserVoiceLine />, color: 'green' },
-  { name: 'BLOG', href: '/Features/eduMentor', icon: <RiArticleLine />, color: 'yellow' },
-  { name: 'Meslek/Erbabı', href: '/Features/eduSocial', icon: <RiTeamLine />, color: 'red' },
+  { name: 'Dinle/Anlat', href: '/Features/eduCard', icon: <RiHeadphoneLine />, color: '#718096' }, // Gri tonları
+  { name: 'Kafam/Karışık', href: '/Features/eduBlog', icon: <RiUserVoiceLine />, color: '#90cdf4' }, // Mavi tonları
+  { name: 'BLOG', href: '/Features/eduMentor', icon: <RiArticleLine />, color: '#f6e05e' }, // Sarı tonları
+  { name: 'Meslek/Erbabı', href: '/Features/eduSocial', icon: <RiTeamLine />, color: '#fc8181' }, // Kırmızı tonları
 ];
 
 function Navbar() {
@@ -18,18 +18,18 @@ function Navbar() {
   };
 
   return (
-    <div className="bg-slate-400 py-5 px-20 w-min mx-auto mt-8 rounded-lg">
-      <ul className="flex justify-center items-center space-x-20 text-lg">
+    <div className="bg-gray-800 py-5 px-20 w-min mx-auto mt-8 rounded-lg "> 
+      <ul className="flex justify-center items-center space-x-20 text-lg ">
         {links.map((link, index) => (
           <li key={index}>
             <Link
-              className={`border flex flex-col items-center ${
+              className={`md:flex-auto hover:scale-110 flex flex-col items-center ${
                 activeLink === index ? 'text-white' : 'text-gray-500'
               }`}
               href={link.href}
               onClick={() => handleLinkClick(index)}
             >
-              {React.cloneElement(link.icon, { className: `text-${link.color}-500 text-2xl` })}
+              {React.cloneElement(link.icon, { className: `text-${link.color} text-2xl` })}
               <span className={`mt-auto`}>{link.name}</span>
             </Link>
           </li>
@@ -40,5 +40,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
-  
